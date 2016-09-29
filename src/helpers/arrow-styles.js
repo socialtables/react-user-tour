9c842d05270a11f468e94b90608f24bff421bab3
Kristoffer Klintberg <krstffr@gmail.com>
2016-09-29 22:02:34 +0200

    Removed some repetition.
    Only tested in chrome dev tools though, so be careful :)

--- src/helpers/arrow-styles.js
+++ src/helpers/arrow-styles.js
@@ -1,45 +1,35 @@
-export const arrowUp = ({color, size}) => {
-	return {
-		width: "0px",
-		height: "0px",
-		borderLeft: `${size}px solid transparent`,
-		borderRight: `${size}px solid transparent`,
-		borderBottom: `${size}px solid ${color}`,
-		position: "absolute",
-		top: `-${size}px`
-	};
+const defaultArrow = {
+  width: '0px',
+  height: '0px',
+  position: 'absolute',
 };
 
-export const arrowDown = ({color, size}) => {
-	return {
-		width: "0px",
-		height: "0px",
-		borderLeft: `${size}px solid transparent`,
-		borderRight: `${size}px solid transparent`,
-		borderTop: `${size}px solid ${color}`,
-		position: "absolute"
-	};
-};
+export const arrowUp = ({ color, size }) =>
+Object.assign({}, defaultArrow, {
+  borderLeft: `${size}px solid transparent`,
+  borderRight: `${size}px solid transparent`,
+  borderBottom: `${size}px solid ${color}`,
+  top: `-${size}px`,
+});
 
-export const arrowRight = ({color, size}) => {
-	return {
-		width: "0px",
-		height: "0px",
-		borderTop: `${size}px solid transparent`,
-		borderBottom: `${size}px solid transparent`,
-		borderLeft: `${size}px solid ${color}`,
-		position: "absolute"
-	};
-};
+export const arrowDown = ({ color, size }) =>
+Object.assign({}, defaultArrow, {
+  borderLeft: `${size}px solid transparent`,
+  borderRight: `${size}px solid transparent`,
+  borderTop: `${size}px solid ${color}`,
+});
 
-export const arrowLeft = ({color, size}) => {
-	return {
-		width: "0px",
-		height: "0px",
-		borderTop: `${size}px solid transparent`,
-		borderBottom: `${size}px solid transparent`,
-		borderRight: `${size}px solid ${color}`,
-		position: "absolute",
-		left: `-${size}px`
-	};
-};
+export const arrowRight = ({ color, size }) =>
+Object.assign({}, defaultArrow, {
+  borderTop: `${size}px solid transparent`,
+  borderBottom: `${size}px solid transparent`,
+  borderLeft: `${size}px solid ${color}`,
+});
+
+export const arrowLeft = ({ color, size }) =>
+Object.assign({}, defaultArrow, {
+  borderTop: `${size}px solid transparent`,
+  borderBottom: `${size}px solid transparent`,
+  borderRight: `${size}px solid ${color}`,
+  left: `-${size}px`,
+});
