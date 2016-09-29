8cd40f478a0a9949eba2d8ee3510b184cfa3c058
Kristoffer Klintberg <krstffr@gmail.com>
2016-09-29 23:00:42 +0200

    fixed comments

--- src/helpers/arrow-styles.js
+++ src/helpers/arrow-styles.js
@@ -1,35 +1,39 @@
 const defaultArrow = {
-  width: '0px',
-  height: '0px',
-  position: 'absolute',
+  width: "0px",
+  height: "0px",
+  position: "absolute",
 };
 
-export const arrowUp = ({ color, size }) =>
-Object.assign({}, defaultArrow, {
-  borderLeft: `${size}px solid transparent`,
-  borderRight: `${size}px solid transparent`,
-  borderBottom: `${size}px solid ${color}`,
-  top: `-${size}px`,
-});
+export const arrowUp = ({ color, size }) => (
+	Object.assign({}, defaultArrow, {
+		borderLeft: `${size}px solid transparent`,
+		borderRight: `${size}px solid transparent`,
+		borderBottom: `${size}px solid ${color}`,
+		top: `-${size}px`,
+	})
+);
 
-export const arrowDown = ({ color, size }) =>
-Object.assign({}, defaultArrow, {
-  borderLeft: `${size}px solid transparent`,
-  borderRight: `${size}px solid transparent`,
-  borderTop: `${size}px solid ${color}`,
-});
+export const arrowDown = ({ color, size }) => (
+	Object.assign({}, defaultArrow, {
+		borderLeft: `${size}px solid transparent`,
+		borderRight: `${size}px solid transparent`,
+		borderTop: `${size}px solid ${color}`,
+	})
+);
 
-export const arrowRight = ({ color, size }) =>
-Object.assign({}, defaultArrow, {
-  borderTop: `${size}px solid transparent`,
-  borderBottom: `${size}px solid transparent`,
-  borderLeft: `${size}px solid ${color}`,
-});
+export const arrowRight = ({ color, size }) => (
+	Object.assign({}, defaultArrow, {
+		borderTop: `${size}px solid transparent`,
+		borderBottom: `${size}px solid transparent`,
+		borderLeft: `${size}px solid ${color}`,
+	})
+);
 
-export const arrowLeft = ({ color, size }) =>
-Object.assign({}, defaultArrow, {
-  borderTop: `${size}px solid transparent`,
-  borderBottom: `${size}px solid transparent`,
-  borderRight: `${size}px solid ${color}`,
-  left: `-${size}px`,
-});
+export const arrowLeft = ({ color, size }) => (
+	Object.assign({}, defaultArrow, {
+		borderTop: `${size}px solid transparent`,
+		borderBottom: `${size}px solid transparent`,
+		borderRight: `${size}px solid ${color}`,
+		left: `-${size}px`,
+	})
+);
