b80f5ac993db84bbc0ed3e027c0a596156c7b51b
Conor Hastings <hastings.conorm@gmail.com>
2017-02-25 11:42:31 -0500

    make sure final position is a whole number

--- src/helpers/position-helpers.js
+++ src/helpers/position-helpers.js
@@ -1,43 +1,43 @@
 const positions = {
 	right: ({position, margin}) => {
 		return {
-			left: position.right + margin,
-			top: position.top + window.pageYOffset,
+			left: Math.floor(position.right + margin),
+			top: Math.floor(position.top + window.pageYOffset),
 			positioned: "right"
 		};
 	},
 	left: ({position, tourElWidth, margin}) => {
 		return {
-			left: (position.left - margin) - tourElWidth,
-			top: position.top + window.pageYOffset,
+			left: Math.floor((position.left - margin) - tourElWidth),
+			top: Math.floor(position.top + window.pageYOffset),
 			positioned: "left"
 		};
 	},
 	top: ({position, tourElHeight, arrowSize, margin}) => {
 		return {
-			left: position.left,
-			top: (position.top + window.pageYOffset) - tourElHeight - arrowSize,
+			left: Math.floor(position.left),
+			top: Math.floor((position.top + window.pageYOffset) - tourElHeight - arrowSize),
 			positioned: "top"
 		};
 	},
 	topLeft: ({position, tourElWidth, tourElHeight, arrowSize, margin}) => {
 		return {
-			left: (position.left + margin) - tourElWidth,
-			top: (position.top + window.pageYOffset) - tourElHeight - arrowSize,
+			left: Math.floor((position.left + margin) - tourElWidth),
+			top: Math.floor((position.top + window.pageYOffset) - tourElHeight - arrowSize),
 			positioned: "topLeft"
 		};
 	},
 	bottom: ({position, arrowSize, offsetHeight, margin}) => {
 		return {
-			left: position.left,
-			top: (position.top + window.pageYOffset) + offsetHeight + arrowSize,
+			left: Math.floor(position.left),
+			top: Math.floor((position.top + window.pageYOffset) + offsetHeight + arrowSize),
 			positioned: "bottom"
 		};
 	},
 	bottomLeft: ({position, tourElWidth, arrowSize, offsetHeight, margin}) => {
 		return {
-			left: (position.left + margin) - tourElWidth,
-			top: (position.top + window.pageYOffset) + offsetHeight + arrowSize,
+			left: Math.floor((position.left + margin) - tourElWidth),
+			top: Math.floor((position.top + window.pageYOffset) + offsetHeight + arrowSize),
 			positioned: "bottomLeft"
 		};
 	}
